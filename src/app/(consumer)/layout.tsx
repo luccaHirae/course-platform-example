fa3c68e2e1fc33d7335fa9d4ea6ccb487a243fe6
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-17 11:53:50 -0300

    feat: add navbar component & layouts

---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link';
+import { SignInButton, UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { Suspense } from 'react';
+import { Button } from '@/components/ui/button';
+
+export default function ConsumerLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Suspense>
+        <Navbar />
+      </Suspense>
+      {children}
+    </>
+  );
+}
+
+async function Navbar() {
+  const user = await currentUser();
+
+  return (
+    <header className='flex h-12 shadow z-10'>
+      <nav className='flex gap-4 custom-container'>
+        <Link
+          href='/'
+          className='mr-auto text-lg hover:underline flex items-center px-2'
+        >
+          Course Platform
+        </Link>
+
+        {user ? (
+          <>
+            <Link
+              href='/admin'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              Admin
+            </Link>
+
+            <Link
+              href='/courses'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              My Courses
+            </Link>
+
+            <Link
+              href='/purchases'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              Purchase History
+            </Link>
+
+            <div className='size-8 self-center'>
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: {
+                      width: '100%',
+                      height: '100%',
+                    },
+                  },
+                }}
+              />
+            </div>
+          </>
+        ) : (
+          <Button className='self-center hover:cursor-pointer' asChild>
+            <SignInButton>Sign In</SignInButton>
+          </Button>
+        )}
+      </nav>
+    </header>
+  );
+}
