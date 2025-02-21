7af69d1730e46aa6e408b6cbb25d6d13b6aa567d
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-21 16:35:43 -0300

    feat: add get & delete course actions

--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -4,8 +4,21 @@ import { redirect } from 'next/navigation';
 import { z } from 'zod';
 import { courseSchema } from '@/schemas/courses';
 import { getCurrentUser } from '@/services/clerk';
-import { canCreateCourses } from '@/features/courses/permissions/courses';
+import {
+  canCreateCourses,
+  canDeleteCourse,
+} from '@/features/courses/permissions/courses';
 import { insertCourse } from '@/features/courses/db/courses';
+import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
+import { getCourseGlobalTag } from '@/features/courses/db/cache/courses';
+import { db } from '@/drizzle/db';
+import {
+  CourseSectionTable,
+  CourseTable,
+  LessonTable,
+  UserCourseAccessTable,
+} from '@/drizzle/schema';
+import { asc, countDistinct, eq } from 'drizzle-orm';
 
 export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const { success, data } = courseSchema.safeParse(unsafeData);
@@ -21,4 +34,50 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const course = await insertCourse(data);
 
   redirect(`/admin/courses/${course.id}/edit`);
+}
+
+export async function deleteCourse(id: string) {
+  const currentUser = await getCurrentUser();
+
+  if (!canDeleteCourse(currentUser)) {
+    return {
+      error: true,
+      message: 'Error deleting course',
+    };
+  }
+
+  // TODO: Delete course from database
+  console.log('Deleting course with id:', id);
+
+  return {
+    error: false,
+    message: 'Successfully deleted course',
+  };
+}
+
+export async function getCourses() {
+  'use cache';
+
+  cacheTag(getCourseGlobalTag());
+
+  return db
+    .select({
+      id: CourseTable.id,
+      name: CourseTable.name,
+      sectionsCount: countDistinct(CourseSectionTable),
+      lessonsCount: countDistinct(LessonTable),
+      studentsCount: countDistinct(UserCourseAccessTable),
+    })
+    .from(CourseTable)
+    .leftJoin(
+      CourseSectionTable,
+      eq(CourseSectionTable.courseId, CourseTable.id)
+    )
+    .leftJoin(LessonTable, eq(LessonTable.sectionId, CourseTable.id))
+    .leftJoin(
+      UserCourseAccessTable,
+      eq(UserCourseAccessTable.courseId, CourseTable.id)
+    )
+    .orderBy(asc(CourseTable.name))
+    .groupBy(CourseTable.id);
 }
