55ec7e5a757f54883fc277f6389497c3cf00b277
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-26 11:33:26 -0300

    fix: product schema typo

--- src/schemas/products.ts
+++ src/schemas/products.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const productSchema = z.object({
   name: z.string().min(1, 'Required'),
-  proceInDollars: z.number().int().nonnegative(),
+  priceInDollars: z.number().int().nonnegative(),
   description: z.string().min(1, 'Required'),
   imageUrl: z.union([
     z.string().url('Invalid URL'),
