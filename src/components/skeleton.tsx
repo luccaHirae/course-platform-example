653f81ff88df8070dd2e7f280400cf5885ba5825
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-05 12:22:22 -0300

    feat: add skeleton components

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
@@ -12,5 +13,41 @@ export function SkeletonButton({ className }: { className?: string }) {
         className
       )}
     ></div>
+  );
+}
+
+export function SkeletonArray({
+  length = 3,
+  children,
+}: {
+  length?: number;
+  children: ReactNode;
+}) {
+  return Array.from({ length }).map(() => children);
+}
+
+export function SkeletonText({
+  rows = 1,
+  size = 'md',
+  className,
+}: {
+  rows?: number;
+  size?: 'md' | 'lg';
+  className?: string;
+}) {
+  return (
+    <div className='flex flex-col gap-1'>
+      <SkeletonArray length={rows}>
+        <div
+          className={cn(
+            'bg-secondary animate-pulse w-full rounded-sm',
+            rows > 1 && 'last:w-3/4',
+            size === 'md' && 'h-3',
+            size === 'lg' && 'h-5',
+            className
+          )}
+        ></div>
+      </SkeletonArray>
+    </div>
   );
 }
