6bb666d75614b4f29a912182eed0f3d098866e8b
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-06 11:42:02 -0300

    feat: add functions to retrieve purchase details and Stripe session information

--- src/features/purchases/db/purchases.ts
+++ src/features/purchases/db/purchases.ts
@@ -1,11 +1,14 @@
+import Stripe from 'stripe';
+import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
 import { db } from '@/drizzle/db';
+import { and, desc, eq } from 'drizzle-orm';
 import { PurchaseTable } from '@/drizzle/schema';
 import {
+  getPurchaseIdTag,
   getPurchaseUserTag,
   revalidatePurchaseCache,
 } from '@/features/purchases/db/cache';
-import { desc, eq } from 'drizzle-orm';
-import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
+import { stripeServerClient } from '@/services/stripe/stripe-server';
 
 export async function insertPurchase(
   data: typeof PurchaseTable.$inferInsert,
@@ -47,4 +50,126 @@ export async function getPurchases(userId: string) {
     where: eq(PurchaseTable.userId, userId),
     orderBy: desc(PurchaseTable.createdAt),
   });
+}
+
+export async function getPurchase({
+  userId,
+  id,
+}: {
+  userId: string;
+  id: string;
+}) {
+  'use cache';
+
+  cacheTag(getPurchaseIdTag(id));
+
+  return await db.query.PurchaseTable.findFirst({
+    columns: {
+      pricePaidInCents: true,
+      refundedAt: true,
+      productDetails: true,
+      createdAt: true,
+      stripeSessionId: true,
+    },
+    where: and(eq(PurchaseTable.id, id), eq(PurchaseTable.userId, userId)),
+  });
+}
+
+export async function getStripeDetails(
+  stripeSessionId: string,
+  pricePaidInCents: number,
+  isRefunded: boolean
+) {
+  const { payment_intent, total_details, amount_total, amount_subtotal } =
+    await stripeServerClient.checkout.sessions.retrieve(stripeSessionId, {
+      expand: [
+        'payment_intent.latest_charge',
+        'total_details.breakdown.discounts',
+      ],
+    });
+
+  const renfundedAmount =
+    typeof payment_intent !== 'string' &&
+    typeof payment_intent?.latest_charge !== 'string'
+      ? payment_intent?.latest_charge?.amount_refunded
+      : isRefunded
+      ? pricePaidInCents
+      : undefined;
+
+  return {
+    receiptUrl: getReceiptUrl(payment_intent),
+    pricingRows: getPricingRows(total_details, {
+      total: (amount_total ?? pricePaidInCents) - (renfundedAmount ?? 0),
+      subtotal: amount_subtotal ?? pricePaidInCents,
+      refund: renfundedAmount,
+    }),
+  };
+}
+
+function getReceiptUrl(payment_intent: Stripe.PaymentIntent | string | null) {
+  if (
+    typeof payment_intent === 'string' ||
+    typeof payment_intent?.latest_charge === 'string'
+  )
+    return;
+
+  return payment_intent?.latest_charge?.receipt_url;
+}
+
+function getPricingRows(
+  totalDetails: Stripe.Checkout.Session.TotalDetails | null,
+  {
+    total,
+    subtotal,
+    refund,
+  }: {
+    total: number;
+    subtotal: number;
+    refund?: number;
+  }
+) {
+  const pricingRows: Array<{
+    label: string;
+    amountInDollars: number;
+    isBold?: boolean;
+  }> = [];
+
+  if (totalDetails?.breakdown != null) {
+    totalDetails.breakdown.discounts.forEach((element) => {
+      pricingRows.push({
+        label: `${element.discount.coupon.name} (${element.discount.coupon.percent_off}$ off)`,
+        amountInDollars: element.amount / -100,
+      });
+    });
+  }
+
+  if (refund) {
+    pricingRows.push({
+      label: 'Refund',
+      amountInDollars: refund / -100,
+    });
+  }
+
+  if (pricingRows.length === 0) {
+    return [
+      {
+        label: 'Total',
+        amountInDollars: total / 100,
+        isBold: true,
+      },
+    ];
+  }
+
+  return [
+    {
+      label: 'Subtotal',
+      amountInDollars: subtotal / 100,
+    },
+    ...pricingRows,
+    {
+      label: 'Total',
+      amountInDollars: total / 100,
+      isBold: true,
+    },
+  ];
 }
