a592af671ef8a79292c5c7b705a3d51e2210ec0e
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-06 12:13:30 -0300

    feat: add revokeUserCourseAccess function to manage user course access based on purchases

--- src/features/courses/db/userCourseAccess.ts
+++ src/features/courses/db/userCourseAccess.ts
@@ -1,5 +1,10 @@
+import { and, eq, inArray, isNull } from 'drizzle-orm';
 import { db } from '@/drizzle/db';
-import { UserCourseAccessTable } from '@/drizzle/schema';
+import {
+  ProductTable,
+  PurchaseTable,
+  UserCourseAccessTable,
+} from '@/drizzle/schema';
 import { revalidateUserCourseAccessCache } from '@/features/courses/db/cache/userCourseAccess';
 
 export async function addUserCourseAccess(
@@ -21,4 +26,68 @@ export async function addUserCourseAccess(
   accesses.forEach(revalidateUserCourseAccessCache);
 
   return accesses;
+}
+
+export async function revokeUserCourseAccess(
+  {
+    userId,
+    productId,
+  }: {
+    userId: string;
+    productId: string;
+  },
+  trx: Omit<typeof db, '$client'> = db
+) {
+  const validPurchases = await trx.query.PurchaseTable.findMany({
+    where: and(
+      eq(PurchaseTable.userId, userId),
+      isNull(PurchaseTable.refundedAt)
+    ),
+    with: {
+      product: {
+        with: {
+          courseProduct: {
+            columns: {
+              courseId: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const refundPurchase = await trx.query.ProductTable.findFirst({
+    where: eq(ProductTable.id, productId),
+    with: {
+      courseProduct: {
+        columns: {
+          courseId: true,
+        },
+      },
+    },
+  });
+
+  if (!refundPurchase) return;
+
+  const validCourseIds = validPurchases.flatMap((purchase) =>
+    purchase.product.courseProduct.map((cp) => cp.courseId)
+  );
+
+  const removeCourseIds = refundPurchase.courseProduct
+    .flatMap((cp) => cp.courseId)
+    .filter((courseId) => !validCourseIds.includes(courseId));
+
+  const revokedAccesses = await trx
+    .delete(UserCourseAccessTable)
+    .where(
+      and(
+        eq(UserCourseAccessTable.userId, userId),
+        inArray(UserCourseAccessTable.courseId, removeCourseIds)
+      )
+    )
+    .returning();
+
+  revokedAccesses.forEach(revalidateUserCourseAccessCache);
+
+  return revokedAccesses;
 }
