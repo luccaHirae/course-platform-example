b8f0fb41b08f6a2cf232465c3f3527ded90b3538
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-10 14:09:11 -0300

    feat: add functions to retrieve lesson details and navigate between previous and next lessons

--- src/features/lessons/actions/lessons.ts
+++ src/features/lessons/actions/lessons.ts
@@ -7,6 +7,7 @@ import {
   canCreateLessons,
   canDeleteLessons,
   canUpdateLessons,
+  wherePublicLessons,
 } from '@/features/lessons/permissions/lessons';
 import {
   editLesson,
@@ -16,10 +17,19 @@ import {
   reorderLessons,
 } from '@/features/lessons/db/lessons';
 import { db } from '@/drizzle/db';
-import { UserLessonCompleteTable } from '@/drizzle/schema';
-import { eq } from 'drizzle-orm';
+import {
+  CourseSectionTable,
+  LessonTable,
+  UserLessonCompleteTable,
+} from '@/drizzle/schema';
+import { and, asc, desc, eq, gt, lt } from 'drizzle-orm';
 import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
-import { getUserLessonCompleteUserTag } from '@/features/lessons/db/cache/userLessonComplete';
+import {
+  getUserLessonCompleteIdTag,
+  getUserLessonCompleteUserTag,
+} from '@/features/lessons/db/cache/userLessonComplete';
+import { getLessonIdTag } from '@/features/lessons/db/cache/lessons';
+import { wherePublicCourseSections } from '@/features/courseSections/permissions/sections';
 
 export async function createLesson(unsafedata: z.infer<typeof lessonSchema>) {
   const { success, data } = lessonSchema.safeParse(unsafedata);
@@ -107,4 +117,155 @@ export async function getCompletedLessonsIds(userId: string) {
   });
 
   return data.map(({ lessonId }) => lessonId);
+}
+
+export async function getLessonDetails(id: string) {
+  'use cache';
+
+  cacheTag(getLessonIdTag(id));
+
+  return db.query.LessonTable.findFirst({
+    where: and(eq(LessonTable.id, id), wherePublicLessons),
+    columns: {
+      id: true,
+      youtubeVideoId: true,
+      name: true,
+      description: true,
+      status: true,
+      sectionId: true,
+      order: true,
+    },
+  });
+}
+
+export async function getIsLessonComplete({
+  lessonId,
+  userId,
+}: {
+  lessonId: string;
+  userId: string;
+}) {
+  'use cache';
+
+  cacheTag(getUserLessonCompleteIdTag({ userId, lessonId }));
+
+  const data = await db.query.UserLessonCompleteTable.findFirst({
+    where: and(
+      eq(UserLessonCompleteTable.userId, userId),
+      eq(UserLessonCompleteTable.lessonId, lessonId)
+    ),
+  });
+
+  return data != null;
+}
+
+export async function getPreviousLesson(lesson: {
+  id: string;
+  sectionId: string;
+  order: number;
+}) {
+  let previousLesson = await db.query.LessonTable.findFirst({
+    where: and(
+      lt(LessonTable.order, lesson.order),
+      eq(LessonTable.sectionId, lesson.sectionId),
+      wherePublicLessons
+    ),
+    orderBy: desc(LessonTable.order),
+    columns: {
+      id: true,
+    },
+  });
+
+  if (previousLesson == null) {
+    const section = await db.query.CourseSectionTable.findFirst({
+      where: eq(CourseSectionTable.id, lesson.sectionId),
+      columns: {
+        order: true,
+        courseId: true,
+      },
+    });
+
+    if (section == null) return;
+
+    const previousSection = await db.query.CourseSectionTable.findFirst({
+      where: and(
+        lt(CourseSectionTable.order, section.order),
+        eq(CourseSectionTable.courseId, section.courseId),
+        wherePublicCourseSections
+      ),
+      orderBy: desc(LessonTable.order),
+      columns: {
+        id: true,
+      },
+    });
+
+    if (previousSection == null) return;
+
+    previousLesson = await db.query.LessonTable.findFirst({
+      where: and(
+        eq(LessonTable.sectionId, previousSection.id),
+        wherePublicLessons
+      ),
+      orderBy: desc(LessonTable.order),
+      columns: {
+        id: true,
+      },
+    });
+  }
+
+  return previousLesson;
+}
+
+export async function getNextLesson(lesson: {
+  id: string;
+  sectionId: string;
+  order: number;
+}) {
+  let nextLesson = await db.query.LessonTable.findFirst({
+    where: and(
+      gt(LessonTable.order, lesson.order),
+      eq(LessonTable.sectionId, lesson.sectionId),
+      wherePublicLessons
+    ),
+    orderBy: asc(LessonTable.order),
+    columns: {
+      id: true,
+    },
+  });
+
+  if (nextLesson == null) {
+    const section = await db.query.CourseSectionTable.findFirst({
+      where: eq(CourseSectionTable.id, lesson.sectionId),
+      columns: {
+        order: true,
+        courseId: true,
+      },
+    });
+
+    if (section == null) return;
+
+    const nextSection = await db.query.CourseSectionTable.findFirst({
+      where: and(
+        gt(CourseSectionTable.order, section.order),
+        eq(CourseSectionTable.courseId, section.courseId),
+        wherePublicCourseSections
+      ),
+      orderBy: asc(LessonTable.order),
+      columns: {
+        id: true,
+      },
+    });
+
+    if (nextSection == null) return;
+
+    nextLesson = await db.query.LessonTable.findFirst({
+      where: and(eq(LessonTable.sectionId, nextSection.id), wherePublicLessons),
+      orderBy: asc(LessonTable.order),
+      columns: {
+        id: true,
+      },
+    });
+  }
+
+  return nextLesson;
 }
